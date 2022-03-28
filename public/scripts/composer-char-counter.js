c85b14e40047dc48dae025310e6577020df934e1
ConsensusAI <malik.iqbal@hotmail.ca>
2022-03-28 00:35:56 +0000

    Turn character counter red if over limit

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -2,9 +2,14 @@ $(document).ready(function () {
   $("#tweet-text").on("input", function () {
     let charCount = $(this).val().length;
     let charsLeft = 140 - charCount;
-    console.log(charCount);
 
     let counter = $(this).parent().next("div").children(".counter");
     counter.text(charsLeft);
+
+    if (charsLeft < 0) {
+      counter.addClass("limitExceeded");
+    } else {
+      counter.removeClass("limitExceeded");
+    }
   });
 });
