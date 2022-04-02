795b5c59b5624bf00e4317cba397482f42afce3d
ConsensusAI <malik.iqbal@hotmail.ca>
2022-04-02 17:36:40 +0000

    Implement loadTweets function to fetch tweets to render

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,31 +4,6 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
-const data = [
-  {
-    user: {
-      name: "Newton",
-      avatars: "https://i.imgur.com/73hZDYK.png",
-      handle: "@SirIsaac",
-    },
-    content: {
-      text: "If I have seen further it is by standing on the shoulders of giants",
-    },
-    created_at: 1461116232227,
-  },
-  {
-    user: {
-      name: "Descartes",
-      avatars: "https://i.imgur.com/nlhLi3I.png",
-      handle: "@rd",
-    },
-    content: {
-      text: "Je pense , donc je suis",
-    },
-    created_at: 1461113959088,
-  },
-];
-
 const createTweetElement = (data) => {
   return `<article class="tweet">
   <header>
@@ -64,13 +39,20 @@ const renderTweet = function (data) {
 };
 
 $(document).ready(function () {
-  renderTweet(data);
-
   $("#submit-new-tweet").submit(function (event) {
     event.preventDefault();
     let data = $(this).serialize();
     console.log(data);
-    // $.ajax("/tweets", { method: "POST", data: data });
     $.post("/tweets", data);
   });
+
+  const loadTweets = () => {
+    console.log("loading..");
+    $.ajax("/tweets", { method: "GET" }).then(function (tweets) {
+      console.log("Success ", tweets);
+      renderTweet(tweets);
+    });
+  };
+
+  loadTweets();
 });
