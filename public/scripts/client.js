f2a289d7a892a6bf7abade63d829dd512ee47ab2
ConsensusAI <malik.iqbal@hotmail.ca>
2022-04-04 21:12:57 +0000

    Update README

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -3,12 +3,15 @@
  * jQuery is already loaded
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
+
+// Function to prevent cross-site scripting
 const escape = (str) => {
   let div = document.createElement("div");
   div.appendChild(document.createTextNode(str));
   return div.innerHTML;
 };
 
+// Creates a new tweet element using passed in data
 const createTweetElement = (data) => {
   return `<article class="tweet">
   <header>
@@ -36,21 +39,26 @@ const createTweetElement = (data) => {
 </article>`;
 };
 
+// Function to render the tweets
 const renderTweet = function (data) {
+  // Empties the container to prevent duplicates
   $("#tweets-container").empty();
 
+  // Iterates over array of tweet objects
   for (let tweet of data) {
     let $tweet = createTweetElement(tweet);
     $("#tweets-container").prepend($tweet);
   }
 };
 
+// Function to fetch tweets and run the render function
 const loadTweets = () => {
   $.ajax("/tweets", { method: "GET" }).then(function (tweets) {
     renderTweet(tweets);
   });
 };
 
+// Waits for DOM to be loaded in
 $(document).ready(function () {
   $("#submit-new-tweet").submit(function (event) {
     event.preventDefault();
@@ -68,13 +76,15 @@ $(document).ready(function () {
         .slideDown(0);
     }
 
+    // Hides the error and removes text
     $(".errors").text("").hide();
 
+    // Convert input to serialized form
     let data = $(this).serialize();
 
     $.post("/tweets", data, function () {
+      // Empties the text area after tweet is submitted
       $("#tweet-text").val("");
-      console.log("reloading");
       loadTweets();
     });
   });
--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,11 +1,17 @@
 $(document).ready(function () {
   $("#tweet-text").on("input", function () {
+    // Define charCount as the length of text in the text area
     let charCount = $(this).val().length;
+    // Count remaining characters as this is what we want to display
     let charsLeft = 140 - charCount;
 
+    // Defines the counter by its location in the DOM
     let counter = $(this).parent().next("div").children(".counter");
+
+    // Inputs the charsLeft as the text in the counter
     counter.text(charsLeft);
 
+    // Handles class changing for character limit
     if (charsLeft < 0) {
       counter.addClass("limitExceeded");
     } else {
